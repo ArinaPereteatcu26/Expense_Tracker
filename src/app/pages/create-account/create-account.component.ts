41e0c7f8aa8686d502ee139c77e3e15c33bc409a
Arina <arinapereteatcu0@gmail.com>
2025-04-19 13:00:19 +0300

    first commit

---
+++ src/app/pages/create-account/create-account.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [ReactiveFormsModule],
+  templateUrl: './create-account.component.html',
+  styleUrl: './create-account.component.css'
+})
+export class CreateAccountComponent {
+  accountForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private userService: UserService, private router: Router) {}
+
+    createAccount()
+    {
+      console.log('Clicked')
+      this.userService.addUser(this.accountForm.value.name);
+      this.router.navigateByUrl('');
+    }
+  }
