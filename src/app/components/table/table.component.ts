f80e5d2fcdc902b440b03cbadb7eaadeaecb04d4
Arina <arinapereteatcu0@gmail.com>
2025-05-07 01:01:19 +0300

    Fix the mobile responsivness

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { UiService } from '../../services/ui.service';
 import { TableDataConfig } from '../../interfaces/ui-config/table-data-config.interface';
 import { DatePipe } from '@angular/common';
@@ -7,7 +7,7 @@ import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [DatePipe],
+  imports: [CommonModule, DatePipe],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
